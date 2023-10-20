d6fea62d3cba797bd84fe3f17201cffbbb9fe2f6
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2023-10-20 17:39:57 -0300

    Initial Commit

---
+++ TodoSwiftData/TodoSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftDataApp.swift
+//  TodoSwiftData
+//
+//  Created by Alliston Aleixo on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
